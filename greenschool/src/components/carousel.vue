ebbe8f9be3d4a851b6a0848e854da6b6a5681f74
shawh <shahfahad11@gmail.com>
2020-04-09 22:50:43 +0200

    fix carousel

--- greenschool/src/components/carousel.vue
+++ greenschool/src/components/carousel.vue
@@ -1,14 +1,40 @@
 <template>
-<v-carousel>
+   <v-flex xs12 lg12 md12 style="width:500%;  margin: auto 0">
+        <v-carousel cycle hide-delimiters style="width:100%; max-height:900px" class="white">
+          <v-carousel-item v-for="(item,i) in items" :src="item.src" :key="i"></v-carousel-item>
+        </v-carousel>
+      </v-flex>
+<!-- <v-card class=mt-2>
+ <v-carousel
+    cycle
+    height="700"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
     <v-carousel-item
+     
       v-for="(item,i) in items"
       :key="i"
       :src="item.src"
-      reverse-transition="fade-transition"
-      transition="fade-transition"
-    ></v-carousel-item>
+    >
+     
+    </v-carousel-item>
   </v-carousel>
-
+</v-card> -->
+<!-- <v-carousel>
+    <v-carousel-item
+  
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+     
+        cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+   ></v-carousel-item>
+  </v-carousel> -->
+ 
 </template>
 
 <script>
@@ -18,16 +44,16 @@
     data: () => ({
      items: [
           {
-            src: 'https://github.com/Shawh1/green-school/blob/master/greenschool/src/assets/1.jpg'
+            src: require('@/assets/1.jpg')
           },
           {
-            src: 'https://github.com/Shawh1/green-school/blob/master/greenschool/src/assets/1.jpg',
+            src: require('@/assets/2.jpg'),
           },
           {
-            src: 'https://github.com/Shawh1/green-school/blob/master/greenschool/src/assets/1.jpg',
+            src: require('@/assets/3.jpg'),
           },
           {
-            src: 'https://github.com/Shawh1/green-school/blob/master/greenschool/src/assets/1.jpg',
+            src: require('@/assets/1.jpg'),
           },
         ],
     }),
